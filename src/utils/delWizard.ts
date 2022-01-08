f4214b5090ecc410d5cf9ea421254d29ffeea483
Luís Felipe de Melo Nunes <luisfelipe5417@gmail.com>
2022-01-08 14:30:04 -0300

    Preview before deleting message

--- src/utils/delWizard.ts
+++ src/utils/delWizard.ts
@@ -1,7 +1,7 @@
 import { Markup } from "telegraf";
 import { composeWizardScene } from "./sceneFactory";
 import { AnyContext, ContextComplement } from "../types/context"
-import { findAllMessage, deleteMessage } from "../db/messageController";
+import { findAllMessage, deleteMessage, findMessage } from "../db/messageController";
 
 
 const exit_keyboard = Markup.keyboard(['🛑 Exit']).oneTime().resize();
@@ -49,12 +49,76 @@ export const createDelWizard = composeWizardScene(
             const msg_keyboard = await getMessagesKeyboard();
             ctx.reply("Message doesn't fit expected result. Try again:", msg_keyboard);
             return ctx.wizard;
-        }
+        }        
         if(message_id != -1 || !message_id){
+            const message = await findMessage(message_id);
+            if(!message){
+                ctx.reply("Mensagem não encontrada, tente novamente.", await getMessagesKeyboard());
+                return ctx.wizard
+            }
+            await ctx.reply(`A mensagem a ser deletada é:`);
+            switch(message?.type){
+                case "text":
+                    await ctx.reply(`${message.caption}`);
+                    break;
+                case "photo":
+                    if (message.fileid)
+                        await ctx.replyWithPhoto(message.fileid, {caption: message.caption || ""}).catch(err => ctx.reply("Failed to fetch photo"));
+                    else
+                        await ctx.reply("Failed to fetch photo");
+                    break;
+                case "audio":
+                    if(message.fileid)
+                        await ctx.replyWithAudio(message.fileid, {caption: message.caption || ""}).catch(err => ctx.reply("Failed to fetch audio"));
+                    else
+                        await ctx.reply("Failed to fetch audio");
+                    break;
+                case "sticker":
+                    if(message.fileid)
+                        await ctx.replyWithSticker(message.fileid).catch(err=> ctx.reply("Failed to fetch sticker"));
+                    else
+                        await ctx.reply("Failed to fetch audio")
+                    break;
+                case "animation":
+                    if(message.fileid)
+                        await ctx.replyWithAnimation(message.fileid, {caption: message.caption || ""}).catch(err=> ctx.reply("Failed to fetch animation"));
+                    else
+                        await ctx.reply("Failed to fetch animation");
+                case "video":
+                    if(message.fileid)
+                        await ctx.replyWithVideo(message.fileid, {caption: message.caption || ""}).catch(err => ctx.reply("Failed to fetch a video"));
+                    else
+                        await ctx.reply("Failed to fetch video");
+            }            
+            const last_keyboard =  Markup.keyboard([
+                ['✅ Sim', '❌ Não'],
+                ['🛑 Exit']
+            ]).oneTime().resize();
+            ctx.reply("Deseja continuar?", last_keyboard);
+        } else ctx.reply(`Failed message_id parsing. got: ${message_id}`);
+        ctx.wizard.state.msg_id = message_id;
+        return ctx.wizard.next();
+
+    }, async (ctx: (AnyContext & ContextComplement), done: any)=> {
+        if(ctx.message.text?.match(/"(❌ Não|✅ Sim|🛑 Exit)"/)){
+            ctx.reply("Unexpected Answer.")
+            return ctx.wizard.back();
+        }
+        if(ctx.message.text?.match(/❌ Não|não|nao/gi)){
+            ctx.reply("Selecione uma mensagem pra remover ou digite o id:", await getMessagesKeyboard());
+            return ctx.wizard.selectStep(0);
+        }
+
+        const message_id = ctx.wizard.state.msg_id;
+        if (!message_id){
+            await ctx.reply("Mensagem não encontrada.");
+            return ctx.wizard.selectStep(0);
+        }
+        if(ctx.message.text?.match(/✅ Sim|sim/gi)){
             const result = await deleteMessage(message_id);
             if(result) ctx.reply("Mensagem deletada com sucesso");
             else ctx.reply(`Mensagem de id ${message_id} não encontrado.`);
-        } else ctx.reply(`Failed message_id parsing. got: ${message_id}`);
+        }
         return done();
     }
 )
